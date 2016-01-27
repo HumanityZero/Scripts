b64bf3752e25f2038a7419f04babad87790d6783
HumanityZero <jackoweese@gmail.com>
2016-01-27 11:13:13 -0500

    Update Taste the rainbow.ps1

--- Taste the rainbow.ps1
+++ Taste the rainbow.ps1
@@ -1,4 +1,7 @@
-﻿$colors = @("Black","Blue","Cyan","DarkBlue","DarkCyan","DarkGray","DarkGreen","DarkMagenta","DarkRed","DarkYellow","Gray","Green","Magenta","Red","White","Yellow")
+#Constantly test path for connection
+
+
+$colors = @("Black","Blue","Cyan","DarkBlue","DarkCyan","DarkGray","DarkGreen","DarkMagenta","DarkRed","DarkYellow","Gray","Green","Magenta","Red","White","Yellow")
 for($y=0; $y -lt 5000){
     for($u = 0; $u -le 15; $u++){
         $test = Test-Path '\\10.51.0.12\Tool Shop\05. Share\$ToolShopReportPkg\ToolShopOrders.xlsm'
